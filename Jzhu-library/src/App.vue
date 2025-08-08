fcaa7b2851d48e2102d2b5b35ab04dd17bf87e27
Zhmzjy <161552291+Zhmzjy@users.noreply.github.com>
2025-08-08 14:03:56 +1000

    finish week 1-2 assignment

---
+++ Jzhu-library/src/App.vue
@@ -0,0 +1,12 @@
+
+<script setup>
+import JSONLab from './assets/components/JSON.vue';
+</script>
+
+
+<template>
+  <div>
+    <JSONLab />
+  </div>
+</template>
+<style scoped></style>
---
+++ Jzhu-library/src/assets/components/Junyiz.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Hello, this is Junyiz</h2>
+    <p>
+      This is my link for GitHub
+      <a
+        href="https://github.com/Zhmzjy/fit5032"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        github.com/Zhmzjy/fit5032
+      </a>
+    </p>
+  </div>
+</template>
+
+
+
+<style scoped>
+h2 { color: #42b983; }
+a { text-decoration: underline; }
+</style>
