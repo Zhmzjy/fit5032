cc279e39e1d45bf55cd3078e43dab4208e30d9cf
Zhmzjy <161552291+Zhmzjy@users.noreply.github.com>
2025-08-08 11:48:41 +1000

    create a vue project

---
+++ fit5032-week1/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <img src="/vite.svg" alt="Vite logo" class="logo" />
+    <img src="./assets/vue.svg" alt="Vue logo" class="logo vue" />
+    <h1>You did it!</h1>
+  </div>
+</template>
+
+<style scoped></style>
