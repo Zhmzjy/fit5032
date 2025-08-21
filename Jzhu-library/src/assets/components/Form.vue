3393429d0687f34dfa329bffe46c6aa17ae061e4
Zhmzjy <161552291+Zhmzjy@users.noreply.github.com>
2025-08-21 16:33:48 +1000

    finish activity5

--- Jzhu-library/src/assets/components/Form.vue
+++ Jzhu-library/src/assets/components/Form.vue
@@ -19,31 +19,52 @@ const submitForm = () => {
 </script>
 
 <template>
-  <div class="form">
-    <h1>User Information Form / Credentials</h1>
-    <form @submit.prevent="submitForm">
-      <label for="username">Username:</label><br>
-      <input type="text" id="username" name="username" v-model="formData.username"><br>
+<div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">User Information Form</h1>
+        <form @submit.prevent="submitForm">
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" id="username" v-model="formData.username">
+            </div>
+            <div class="col-md-6">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password" v-model="formData.password">
+            </div>
+          </div>
 
-      <label for="password">Password:</label><br>
-      <input type="password" id="password" name="password" v-model="formData.password"><br>
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="isAustralian"
+                       v-model="formData.isAustralian">
+                <label class="form-check-label" for="isAustralian">Australian Resident?</label>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <label for="gender" class="form-label">Gender</label>
+              <select class="form-select" id="gender" v-model="formData.gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+          </div>
 
-      <label for="isAustralian">Australian Resident?</label><br>
-      <input type="checkbox" id="isAustralian" name="isAustralian" v-model="formData.isAustralian"><br>
+          <div class="mb-3">
+            <label for="reason" class="form-label">Reason for joining</label>
+            <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
+          </div>
 
-      <label for="reason">Reason For Joining:</label><br>
-      <textarea id="reason" name="reason" rows="3" v-model="formData.reason"></textarea><br>
-
-      <label for="gender">Gender</label><br>
-      <select id="gender" v-model="formData.gender">
-        <option value="" disabled>Select…</option>
-        <option value="female">Female</option>
-        <option value="male">Male</option>
-        <option value="other">Other</option>
-      </select>
-      <br>
-      <button type="submit">Submit</button>
-    </form>
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary me-2">Submit</button>
+            <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 
   <div class="row mt-5" v-if="submittedCards.length">
