9937a368002e8578d77409727a08b85e92360115
Zhmzjy <161552291+Zhmzjy@users.noreply.github.com>
2025-09-05 22:44:36 +1000

    finish week5 activity 5.3.1

--- Jzhu-library/src/main.js
+++ Jzhu-library/src/main.js
@@ -1,25 +1,19 @@
+// import './assets/main.css'
+// import '@/assets/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+// import DataTable from 'primevue/datatable'
+// import Column from 'primevue/Column'
 
-const app = createApp(App);
+const app = createApp(App)
+app.use(PrimeVue, { theme: { preset: Aura } })
 
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-    options: {
-      darkModeSelector: false
-    }
-  }
-});
-app.component('DataTable', DataTable)
-app.component('Column', Column)
+// app.component('DataTable', DataTable)
+// app.component('Column', Column)
 
-app.mount('#app');
-
-
+app.mount('#app')
